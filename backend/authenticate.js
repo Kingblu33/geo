42cab37746c6dcf5fda7cb30d7636349c0a4feed
Kingblu33 <demmarblue@gmail.com>
2024-03-13 00:53:42 -0400

    removedFile

--- backend/authenticate.js
+++
@@ -1,9 +0,0 @@
-const authentication = {
-        credentials: {
-            database: 'go_penske',
-            userName: 'api@installcheck',
-            password: 'Gbmj45892!@dfKIJvfk5mHF'
-        },
-        path: 'my.geotab.com'
-    };
-
