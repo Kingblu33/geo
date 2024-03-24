c67d5b5360b1cdaebad7dbe0d2771d2ee9a98143
Kingblu33 <demmarblue@gmail.com>
2024-03-24 14:02:49 -0400

    ctr

--- backend/test.js
+++ backend/test.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const GeotabApi = require('mg-api-js');
-// const authenticate = require('./authenticate')
 require('dotenv').config()
 const app = express();
 
@@ -15,19 +14,29 @@ const authentication = {
 
 
 const api = new GeotabApi(authentication)
+
+
 api.authenticate( success => {
-    console.log('Successful authentication');
+    // const apiKey = api._helper.credentialStore.credentials_key;
+    // const sessionId = api._helper.credentialStore.server_key;
+    console.log(api)
 }, (message, error) => {
     console.log('Something went wrong',error);
+    console.log(message)
 });
 
 app.get('/',((req,res)=>{
     res.redirect('/test');
 }))
 
-app.get('/test', async (req, res) => {
+app.get('/getdevices', async (req, res) => {
     try {
-        const devices = await api.call('Get', { typeName: 'Device', resultsLimit: 100});
+        const devices = await api.call('Get', 
+        { 
+            typeName: 'Device',
+            resultsLimit: 100
+        }
+    );
         res.json(devices);
     } catch (error) {
         console.error('Err', error);
@@ -35,78 +44,59 @@ app.get('/test', async (req, res) => {
     }
 });
 
-// latitude	38.7128334
-// longitude	-121.303268
-// speed	74
-// dateTime	"2018-02-14T01:06:55.000Z"
-// device	
-// id	"b4105"
-// id	"bDA88171C"
-// Test Devices
-// Vin: 3HAEUMML5ML812730
-//Serial Number: G9DE210C699E 
-// 3HSDZAPR0PN464738
-app.get('/test2/:vin', async (req,res)=>{
-    const VIN = req.params.vin;
-    try{
-        const device = await api.call("Get", {
-            "typeName": "Device",
+app.get('/test2/:device', async (req, res) => {
+    const device = req.params.device;
+    try {
+        const result = await api.call("Get", {
+            "typeName": "LogRecord",
             "resultsLimit": 1, 
-            "search": {
-                "vehicleIdentificationNumber": VIN  
-            },
-            // "propertySelector": {
-            //     "fields": ["id", "name"]  
-            // }
-        });;
-        res.json(device)
-    }catch{
-        console.error('Err',error);
-        res.status(500).json({ error: 'Server Err' })
+            "deviceSearch": {
+                "serialNumber": device
+            }
+        });
+        res.json(result);
+    } catch (error) {
+        console.error('Err', error);
+        res.status(500).json({ error: 'Server Err' });
     }
-})
+});
 
 app.get('/test3', async (req,res)=>{
+    
+    const apiKey = api._helper.credentialStore.credentials_key;
+    const sessionId = api._helper.credentialStore.userId;
     try{
-        const device = await api.call("Get", {
-            "typeName": "Device",
+        const device = await api.call("LookupDevice", {
+            "apiKey":apiKey,
+            "sessionId": sessionId,
             "resultsLimit": 1, 
-            "search": {
-                // "vehicleIdentificationNumber": "3HSDZAPR0PN464738"  
-            "id": "b1DB88"
-            },
-            // "propertySelector": {
-            //     "fields": ["id", "name"]  
-            // }
-        });;
+            "serialNo": "G9902108E954",
+            
+        });
         res.json(device)
-    }catch{
+    }catch(error){
         console.error('Err',error);
         res.status(500).json({ error: 'Server Err' })
     }
 })
 
-//b1952D
-//Note: Log record gets the datetime for the vehicle by the id.
-app.get('/test4', async (req, res) => {
+// b1952D
+// Note: Log record gets the datetime for the vehicle by the id.
+app.get('/test4/:serialNo', async (req, res) => {
+    const serialNumber = req.params.serialNo
     try {
         // const deviceId = "bD25D"; 
-        const request = {
-            "typeName": "LookupDevice",
+        
+        const device = await api.call("Get", 
+        {
+            "typeName": "DeviceStatusInfo",
             "resultsLimit": 1, 
             "search": {
-                // "id": deviceId 
-                //b19B11 ass 3HSDZTZR2PN105383
-                //b21497 : associated with 7GZ37TC71MN007764
-                //b1DB88 : associated with 3HAEUMML6RL770737
-                 "deviceSearch" : {
-                    // "id": "b19B11"
-                    "serialNumber": "G9URRM7063DX" 
+                "deviceSearch" : {
+                    "serialNumber": serialNumber
                 } 
-
             }
-        };
-        const device = await api.call("Get", request);
+    });
         res.json(device);
     } catch (error) {
         console.error('Err', error);
@@ -114,36 +104,7 @@ app.get('/test4', async (req, res) => {
     }
 });
 
-
-
-// 3AKJHLDVXPSNS9899
-app.get('/device', async (req, res) => {
-    try {
-        let fromVersion = null;
-        api.call("Get", {
-            "typeName": "LogRecord",
-            "resultsLimit": 1,
-            // "fromVersion": fromVersion,
-            "search": {
-                "vehicleIdentificationNumber": "3AKJGED60FDGL6005"  
-                // "deviceSearch" : {
-                //     "id": "b1DB88"
-                // }
-            }
-        }, result => {
-            fromVersion = result.toVersion;
-            res.json(result.data);
-        }, error => {
-            console.error('Error:', error);
-            res.status(500).json({ error: 'Server Error' });
-        });
-    } catch (error) {
-        console.error('Err', error);
-        res.status(500).json({ error: 'Server Err' });
-    }
-});
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
